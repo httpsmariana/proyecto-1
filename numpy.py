1a392a6d2ef62a25a3ad369226faee807332cc6e
httpsmariana <108203261+httpsmariana@users.noreply.github.com>
2022-09-14 00:07:46 -0500

    Rename nunpy.py to numpy.py

---
+++ numpy.py
@@ -0,0 +1,2 @@
+import numpy as np
+my_data = np.getfromtxt( 'poligono_de_medellin.csv' , delimiter=' ; ' )
--- nunpy.py
+++
@@ -1,2 +0,0 @@
-import numpy as np
-my_data = np.getfromtxt( 'poligono_de_medellin.csv' , delimiter=' ; ' )
